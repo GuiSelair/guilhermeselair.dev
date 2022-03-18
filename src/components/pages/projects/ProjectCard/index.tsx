0c3bcf8712f4f9211fa0557156dd7c05ae4f3a4c
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-03-18 00:31:02 -0300

    Update: ProjectCard props

--- src/components/pages/projects/ProjectCard/index.tsx
+++ src/components/pages/projects/ProjectCard/index.tsx
@@ -1,39 +1,47 @@
 import Image from "next/image";
+import Link from "next/link";
+import { memo } from "react";
 
 import styles from "./styles.module.scss";
 
 interface IProjectCard {
+	projectId: string;
 	projectImage: string;
 	projectName: string;
 	projectType: "web" | "mobile" | "desktop";
 	projectTechologies: string[];
 }
 
-export default function ProjectCard({
+function ProjectCard({
+  projectId,
   projectImage,
   projectName,
   projectType,
   projectTechologies,
 }: IProjectCard) {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <Image
-          src={projectImage}
-          alt={projectName}
-          width={200}
-          height={140}
-          layout="responsive"
-          objectFit="cover"
-        />
-      </div>
-      <div className={styles.detailContainer}>
-        <span>{projectName}</span>
-        <div className={styles.label}>
-          web
+    <Link href={`/project/${projectId}`}>
+      <a className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={projectImage}
+            alt={projectName}
+            width={200}
+            height={140}
+            layout="responsive"
+            objectFit="cover"
+          />
         </div>
-      </div>
-      <div className={styles.techologiesContainer} />
-    </div>
+        <div className={styles.detailContainer}>
+          <span>{projectName}</span>
+          <div className={styles.label}>
+            {projectType}
+          </div>
+        </div>
+        <div className={styles.techologiesContainer} />
+      </a>
+    </Link>
   );
 }
+
+export default memo(ProjectCard);
