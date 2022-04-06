4cdc0653c872ca9859fc4b1c8ac55fffdd58b05d
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-04-06 18:02:30 -0300

    Refactor: Separate Carousel in new component - ProjectCarousel

---
+++ src/components/pages/project/ProjectCarousel/index.tsx
@@ -0,0 +1,100 @@
+import { Carousel } from "react-responsive-carousel";
+import { isMobile } from "react-device-detect";
+import { HiArrowNarrowLeft, HiArrowNarrowRight } from "react-icons/hi";
+import Image from "next/image";
+import { memo } from "react";
+
+import style from "./styles.module.scss";
+
+type IProjectCarousel = {
+	gallery: {
+		src: string;
+		alt?: string;
+		id: string;
+	}[]
+};
+
+function ProjectCarousel({ gallery }: IProjectCarousel) {
+  const hasProjectGalleryImages = gallery.length > 1;
+
+  if (!hasProjectGalleryImages) {
+    return (
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+        showArrows={false}
+        className={style.carouselContainer}
+      >
+        {gallery.map((image) => (
+          <div className={style.carouselShowOnlyCoverContainer} key={image.id}>
+            <Image
+              src={image.src}
+              alt={image.alt}
+              layout="responsive"
+              priority
+              width={100}
+              height={100}
+            />
+          </div>
+        ))}
+      </Carousel>
+    );
+  }
+
+  return (
+    <Carousel
+      centerMode={!isMobile}
+      centerSlidePercentage={!isMobile ? 60 : 65}
+      autoPlay
+      infiniteLoop={!isMobile}
+      showThumbs={false}
+      showArrows
+      showStatus={false}
+      showIndicators
+      className={style.carouselContainer}
+      renderArrowNext={(clickHandler) => (
+        <button
+          className={style.nextImageArrowContainer}
+          type="button"
+          onClick={clickHandler}
+        >
+          <HiArrowNarrowRight />
+        </button>
+      )}
+      renderArrowPrev={(clickHandler) => (
+        <button
+          className={style.previousImageArrowContainer}
+          type="button"
+          onClick={clickHandler}
+        >
+          <HiArrowNarrowLeft />
+        </button>
+      )}
+      renderIndicator={(clickHandler, isSelected, index) => (
+        <button
+          className={style.dot}
+          onClick={clickHandler}
+          data-selected={String(isSelected)}
+          type="button"
+          aria-label={`Indicador ${index}`}
+        />
+      )}
+				>
+      {gallery.map((image) => (
+        <div className={style.carouselImagesContainer} key={image.id}>
+          <Image
+            src={image.src}
+            alt={image.alt}
+            layout="responsive"
+            priority
+            width={100}
+            height={100}
+          />
+        </div>
+      ))}
+    </Carousel>
+  );
+}
+
+export default memo(ProjectCarousel);
--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -2,15 +2,14 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useMemo } from "react";
-import { isMobile } from "react-device-detect";
-import { Carousel } from "react-responsive-carousel";
 import ReactMarkdown from "react-markdown";
-import { HiArrowNarrowLeft, HiArrowNarrowRight, HiOutlineExternalLink } from "react-icons/hi";
+import { HiOutlineExternalLink } from "react-icons/hi";
 import { AiFillGithub } from "react-icons/ai";
 
 import { graphSDK } from "services/graphql-request";
 import { ProjectQuery } from "generated/sdk";
 import SEO from "components/shared/SEO";
+import ProjectCarousel from "components/pages/project/ProjectCarousel";
 import Header from "components/shared/Header";
 import Footer from "components/shared/Footer";
 import style from "styles/pages/Project.module.scss";
@@ -18,11 +17,16 @@ import { getThumbnailFilename } from "utils/getThumbnailFilename";
 
 export default function Project({ project }: ProjectQuery) {
   const projectGallery = useMemo(
-    () => [project.cover.url, ...project.gallery.map((image) => image.url)],
+    () => [
+      {
+        src: project.cover.url,
+        id: project.cover.id,
+      }, ...project.gallery.map((image) => ({
+        src: image.url,
+        id: image.id,
+      }))],
     [project],
   );
-
-  const hasProjectGalleryImages = project.gallery.length;
 
   return (
     <>
@@ -34,81 +38,7 @@ export default function Project({ project }: ProjectQuery) {
       <Header />
       <div className={style.container}>
         <h1>{project.name}</h1>
-        {hasProjectGalleryImages
-          ? (
-            <Carousel
-              centerMode={!isMobile}
-              centerSlidePercentage={!isMobile ? 60 : 65}
-              autoPlay
-              infiniteLoop={!isMobile}
-              showThumbs={false}
-              showArrows
-              showStatus={false}
-              showIndicators
-              className={style.carouselContainer}
-              renderArrowNext={(clickHandler) => (
-                <button
-                  className={style.nextImageArrowContainer}
-                  type="button"
-                  onClick={clickHandler}
-                >
-                  <HiArrowNarrowRight />
-                </button>
-              )}
-              renderArrowPrev={(clickHandler) => (
-                <button
-                  className={style.previousImageArrowContainer}
-                  type="button"
-                  onClick={clickHandler}
-                >
-                  <HiArrowNarrowLeft />
-                </button>
-              )}
-              renderIndicator={(clickHandler, isSelected, index) => (
-                <button
-                  className={style.dot}
-                  onClick={clickHandler}
-                  data-selected={String(isSelected)}
-                  type="button"
-                  aria-label={`Indicador ${index}`}
-                />
-              )}
-            >
-              {projectGallery.map((projectImage) => (
-                <div className={style.carouselImagesContainer} key={projectImage}>
-                  <Image
-                    src={projectImage}
-                    layout="responsive"
-                    priority
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              ))}
-
-            </Carousel>
-          )
-          : (
-            <Carousel
-              showThumbs={false}
-              showStatus={false}
-              showIndicators={false}
-              showArrows={false}
-              className={style.carouselContainer}
-            >
-              {projectGallery.map((projectImage) => (
-                <div className={style.carouselShowOnlyCoverContainer} key={projectImage}>
-                  <Image
-                    src={projectImage}
-                    layout="responsive"
-                    priority
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              ))}
-            </Carousel>
-          )}
+        <ProjectCarousel gallery={projectGallery} />
 
         <div className={style.content}>
           <h2>DESCRIÇÃO</h2>
@@ -123,7 +53,7 @@ export default function Project({ project }: ProjectQuery) {
               <h3>PRINCIPAIS TECNOLOGIAS UTILIZADAS</h3>
               <ul>
                 {project.technologies.map((technology) => (
-                  <li>
+                  <li key={technology}>
                     <Image
                       src={`/images/thumbnails/${getThumbnailFilename(technology)}`}
                       width={30}
