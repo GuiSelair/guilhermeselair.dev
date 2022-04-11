180d0d3c4a8af194e2c5ac192fa870f9995afa67
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-04-10 22:21:36 -0300

    Update: Added Header route logic

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -8,28 +8,33 @@ import style from "./style.module.scss";
 function Header() {
   const router = useRouter();
 
+  const isSubPage = router.pathname.includes("/project/");
+
   return (
     <header className={style.container}>
       <div className={style.leftSide}>
-        <Link href="/">
-          <a>S</a>
-        </Link>
-        {/* <button type="button" title="Voltar" onClick={() => router.back()}>
-          <HiArrowNarrowLeft />
-        </button> */}
+        {isSubPage ? (
+          <button type="button" title="Voltar" onClick={() => router.back()}>
+            <HiArrowNarrowLeft />
+          </button>
+        ) : (
+          <Link href="/">
+            <a>S</a>
+          </Link>
+        )}
       </div>
       <nav className={style.centerNav}>
         <ul>
           <li>
             <Link href="/about">
-              <a className={style.isSelected}>
+              <a className={router.pathname === "/about" ? style.isSelected : ""}>
                 <span>SOBRE</span>
               </a>
             </Link>
           </li>
           <li>
             <Link href="/projects">
-              <a>
+              <a className={router.pathname === "/projects" ? style.isSelected : ""}>
                 <span>PROJETOS</span>
               </a>
             </Link>
@@ -49,7 +54,7 @@ function Header() {
       <div className={style.rightSide}>
         <a
           title="Entrar em contato por Whatsapp"
-          href="https://www.api.whatsapp.com"
+          href="https://www.api.whatsapp.com/"
           target="_blank"
           rel="noreferrer"
         >
