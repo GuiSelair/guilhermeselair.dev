21cae32e30fc1ecffc0dd47c748da909a16e4f2b
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-04-24 23:23:21 -0300

    Update: Changed about page, added apresentation section

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,199 +1,80 @@
-import Head from "next/head";
-import { CgArrowLongDown } from "react-icons/cg";
-import Image from "next/image";
-import {
-  Tab, Tabs, TabList, TabPanel,
-} from "react-tabs";
+import Head from 'next/head'
+import { CgArrowLongDown } from 'react-icons/cg'
+import Image from 'next/image'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
-import styles from "@styles/About.module.scss";
-import Header from "components/shared/Header";
-import Label from "components/shared/Label";
-import ProfessionalExperiencesSection from "components/pages/about/ProfessionalExperiencesSection";
-import DownloadProfessionalResumeBanner from "components/pages/about/DownloadProfessionalResumeBanner";
-import TechnologiesWorked from "components/pages/about/TechnologiesWorked";
+import styles from '@styles/About.module.scss'
+import Header from 'components/shared/Header'
+import Label from 'components/shared/Label'
+import ProfessionalExperiencesSection from 'components/pages/about/ProfessionalExperiencesSection'
+import DownloadProfessionalResumeBanner from 'components/pages/about/DownloadProfessionalResumeBanner'
+import TechnologiesWorked from 'components/pages/about/TechnologiesWorked'
+import SEO from 'components/shared/SEO'
+import CoursesAndCertifications from 'components/pages/about/CoursesAndCertifications'
+import Educations from 'components/pages/about/Educations'
 
 export default function About() {
-  return (
-    <div className={styles.backgroundWrapper}>
-      <Head>
-        <title>Sobre mim - Guilherme Selair</title>
-      </Head>
-      <Header />
-      <main className={styles.container}>
-        <h2>
-          Construindo a melhor
-          <b> experiência </b>
-          para seus clientes!
-        </h2>
+	return (
+		<div className={styles.backgroundWrapper}>
+			<SEO title="Sobre mim" />
+			<Header />
+			<main className={styles.container}>
+				<h2>Always learning!</h2>
+				<div className={styles.apresentationContainer}>
+					<Image
+						src={'https://github.com/guiselair.png'}
+						width={100}
+						height={100}
+						layout="responsive"
+						className={styles.apresentationImage}
+					/>
+					<div className={styles.apresentationDetails}>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est
+							pellentesque velit augue elementum condimentum arcu aliquet
+							adipiscing sagittis.
+						</p>
+						<p>
+							Habitant erat faucibus maecenas nulla scelerisque. Sit
+							sollicitudin fringilla sed nullam ut mi. Faucibus habitant
+							eleifend euismod praesent suscipit et mauris adipiscing pretium.
+						</p>
+						<p>
+							Habitant erat faucibus maecenas nulla scelerisque. Sit
+							sollicitudin fringilla sed nullam ut mi.
+						</p>
+					</div>
+				</div>
 
-        <div className={styles.arrowSVG}>
-          <CgArrowLongDown />
-        </div>
+				<div className={styles.aboutContainerDesktop}>
+					<CoursesAndCertifications />
+					<Educations />
+					<TechnologiesWorked />
+					<ProfessionalExperiencesSection />
+				</div>
 
-        <div className={styles.aboutContainerDesktop}>
-          <section className={styles.declaracoesPessoais}>
-            <h4>Declarações pessoais</h4>
-            <p>
-              Trabalho com programação desde 2018 e já desenvolvi diversos
-              projetos web que impactaram mais de 3000 pessoas. Dentro da área,
-              experiência do usuário e estatisticas sobre desempenho me
-              interessam.
-            </p>
-          </section>
-          <section className={styles.cursosCertificacoes}>
-            <h4>Cursos e Certificações</h4>
-            <ul>
-              <li>
-                <strong>Ignite - Trilha React</strong>
-                <span>Rocketseat</span>
-                <span>Conclusão: 2021</span>
-              </li>
-              <li>
-                <strong>Bootcamp GoStack</strong>
-                <span>Rocketseat</span>
-                <span>Conclusão: 2021</span>
-              </li>
-              <li>
-                <strong>JavaScript - Curso COMPLETO</strong>
-                <span>HCode (Udemy)</span>
-                <span>Conclusão: 2020</span>
-              </li>
-            </ul>
-          </section>
-          <section className={styles.formacaoEducacional}>
-            <h4>Formação Educacional</h4>
-            <ul>
-              <li>
-                <strong>
-                  Tecnólogo em Redes de Computadores
-                  <Label text="Ensino Superior" />
-                </strong>
-                <span>Universidade Federal de Santa Maria</span>
-              </li>
-              <li>
-                <strong>
-                  Técnico em Informática
-                  <Label text="Ensino Técnico" />
-                </strong>
-                <span>
-                  Escola Estadual de Ensino Médio Professora Maria Rocha
-                </span>
-              </li>
-            </ul>
-          </section>
-          <TechnologiesWorked />
-          <ProfessionalExperiencesSection />
-        </div>
+				<div className={styles.aboutContainerMobile}>
+					<Tabs className={styles.aboutContent}>
+						<TabList>
+							<Tab>Mais detalhes</Tab>
+							<Tab>Carreira</Tab>
+						</TabList>
 
-        <div className={styles.aboutContainerMobile}>
-          <Tabs className={styles.aboutContent}>
-            <TabList>
-              <Tab>Sobre mim</Tab>
-              <Tab>Minhas experiências</Tab>
-            </TabList>
+						<TabPanel>
+							<CoursesAndCertifications />
+							<Educations />
+							<TechnologiesWorked />
+						</TabPanel>
 
-            <TabPanel>
-              <section className={styles.declaracoesPessoais}>
-                <h4>Declarações pessoais</h4>
-                <p>
-                  Trabalho com programação desde 2018 e já desenvolvi diversos
-                  projetos web que impactaram mais de 3000 pessoas. Dentro da área,
-                  experiência do usuário e estatisticas sobre desempenho me
-                  interessam.
-                </p>
-              </section>
-              <section className={styles.cursosCertificacoes}>
-                <h4>Cursos e Certificações</h4>
-                <ul>
-                  <li>
-                    <strong>Ignite - Trilha React</strong>
-                    <span>Rocketseat</span>
-                    <span>Conclusão: 2021</span>
-                  </li>
-                  <li>
-                    <strong>Bootcamp GoStack</strong>
-                    <span>Rocketseat</span>
-                    <span>Conclusão: 2021</span>
-                  </li>
-                  <li>
-                    <strong>JavaScript - Curso COMPLETO</strong>
-                    <span>HCode (Udemy)</span>
-                    <span>Conclusão: 2020</span>
-                  </li>
-                </ul>
-              </section>
-              <section className={styles.formacaoEducacional}>
-                <h4>Formação Educacional</h4>
-                <ul>
-                  <li>
-                    <strong>
-                      Tecnólogo em Redes de Computadores
-                      <Label text="Ensino Superior" />
-                    </strong>
-                    <span>Universidade Federal de Santa Maria</span>
-                  </li>
-                  <li>
-                    <strong>
-                      Técnico em Informática
-                      <Label text="Ensino Técnico" />
-                    </strong>
-                    <span>
-                      Escola Estadual de Ensino Médio Professora Maria Rocha
-                    </span>
-                  </li>
-                </ul>
-              </section>
-              <section className={styles.tecnologiasTrabalhadas}>
-                <h4>Tecnologias trabalhadas</h4>
-                <div>
-                  <Image
-                    src="/images/thumbnails/Typescript-Thumb.png"
-                    width={49}
-                    height={49}
-                    title="Typescript"
-                  />
-                  <Image
-                    src="/images/thumbnails/React-Thumb.png"
-                    width={49}
-                    height={49}
-                    title="ReactJS"
-                  />
-                  <Image
-                    src="/images/thumbnails/NextJS-Thumb.png"
-                    width={49}
-                    height={49}
-                    title="NextJS"
-                  />
-                  <Image
-                    src="/images/thumbnails/Typescript-Thumb.png"
-                    width={49}
-                    height={49}
-                    title="Typescript"
-                  />
-                  <Image
-                    src="/images/thumbnails/Typescript-Thumb.png"
-                    width={49}
-                    height={49}
-                    title="Typescript"
-                  />
-                  <Image
-                    src="/images/thumbnails/Typescript-Thumb.png"
-                    width={49}
-                    height={49}
-                    title="Typescript"
-                  />
-                </div>
-              </section>
-            </TabPanel>
-            <TabPanel>
-              <ProfessionalExperiencesSection />
-            </TabPanel>
-          </Tabs>
-        </div>
+						<TabPanel>
+							<ProfessionalExperiencesSection />
+						</TabPanel>
+					</Tabs>
+				</div>
 
-        <DownloadProfessionalResumeBanner />
-      </main>
-      {/* <span>© Guilherme Selair. 2021</span> */}
-    </div>
-  );
+				<DownloadProfessionalResumeBanner />
+			</main>
+			{/* <span>© Guilherme Selair. 2021</span> */}
+		</div>
+	)
 }
