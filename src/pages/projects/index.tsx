c925d9bfc811b8a069b5610844a1100e45b7d22b
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-04-25 22:43:31 -0300

    Update: Projects page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,86 +1,98 @@
-import { CgArrowLongDown } from 'react-icons/cg';
 import { useMemo } from "react";
 import { GetStaticProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { CgArrowLongDown } from "react-icons/cg";
 
-import SEO from 'components/shared/SEO';
-import styles from 'styles/pages/Projects.module.scss';
-import Header from 'components/shared/Header';
-import ProjectCard from 'components/pages/projects/ProjectCard';
-import BannerWithCTA from 'components/shared/BannerWithCTA';
+import SEO from "components/shared/SEO";
+import styles from "styles/pages/Projects.module.scss";
+import Header from "components/shared/Header";
+import ProjectCard from "components/pages/projects/ProjectCard";
+import BannerWithCTA from "components/shared/BannerWithCTA";
 import { graphSDK } from "services/graphql-request";
 import { ProjectsQuery } from "generated/sdk";
 
 type IProjectChunk = ProjectsQuery["projects"][];
 
 export default function Projects({ projects }: ProjectsQuery) {
-  const projectsChunk = useMemo<IProjectChunk>(() => {
-    const projectsBlocks = [];
-    const projectPerBlock = 6;
-    const chunkLimit = {
-      start: 0,
-      end: projectPerBlock,
-    };
-    const blocksOfProjects = Math.ceil(projects.length / projectPerBlock);
+	const projectsChunk = useMemo<IProjectChunk>(() => {
+		const projectsBlocks = [];
+		const projectPerBlock = 6;
+		const chunkLimit = {
+			start: 0,
+			end: projectPerBlock,
+		};
+		const blocksOfProjects = Math.ceil(projects.length / projectPerBlock);
+		for (let i = 0; i < blocksOfProjects; i++) {
+			projectsBlocks.push(projects.slice(chunkLimit.start, chunkLimit.end));
+			chunkLimit.start += projectPerBlock;
+		}
+		return projectsBlocks;
+	}, [projects]);
 
-    for (let i = 0; i < blocksOfProjects; i++) {
-      projectsBlocks.push(projects.slice(chunkLimit.start, chunkLimit.end));
-      chunkLimit.start += projectPerBlock;
-    }
+	const hasProject = projectsChunk.length > 0;
 
-    return projectsBlocks;
-  }, [projects]);
+	const highlightProject = projects[0];
 
-  return (
-    <>
-      <SEO title="Projetos" />
-      <div className={styles.backgroundContainer}>
-        <Header />
-        <main className={styles.container}>
-          <h1>
-            Em busca
-            <br />
-            do próximo
-            <b> nível!</b>
-          </h1>
-          <div className={styles.arrowSVG}>
-            <CgArrowLongDown />
-          </div>
+	return (
+		<>
+			<SEO title="Projetos" />
+			<div className={styles.backgroundContainer}>
+				<Header />
+				<main className={styles.container}>
+					<h1>Em busca do próximo nível!</h1>
+					<Link href={`/project/${highlightProject.slug}`}>
+						<a className={styles.highlightContainer}>
+							<Image
+								src={highlightProject.cover.url}
+								width={100}
+								height={100}
+								layout="responsive"
+								className={styles.highlightImage}
+							/>
+							<div className={styles.highlightBadge}>DESTAQUE</div>
+						</a>
+					</Link>
 
-          <div className={styles.projectContainer}>
-            {projectsChunk[0].map((project) => (
-              <ProjectCard
-                key={project.slug}
-                projectId={project.slug}
-                projectImage={project.cover.url}
-                projectName={project.name}
-                projectType={project.type}
-                projectTechologies={project.technologies}
-              />
-            ))}
-          </div>
-          <BannerWithCTA
-            CTAAction={() => {}}
-            CTAText="Entrar em contato"
-            title="Procurando um desenvolvedor front-end?"
-            description="Seus problemas acabaram!"
-            backgroundColor="#2D2C2A"
-            backgroundImage="message-icon.png"
-            backgroundPositionX="3%"
-            backgroundPositionY="110%"
-          />
+					<div className={styles.arrowSVG}>
+						<CgArrowLongDown />
+					</div>
 
-        </main>
-      </div>
-    </>
-  );
+					<div className={styles.projectContainer}>
+						{hasProject &&
+							projectsChunk[0].map((project) => (
+								<ProjectCard
+									key={project.slug}
+									projectId={project.slug}
+									projectImage={project.cover.url}
+									projectName={project.name}
+									projectType={project.type}
+									projectTechologies={project.technologies}
+								/>
+							))}
+					</div>
+					<BannerWithCTA
+						CTAAction={() => {}}
+						CTAText="Entrar em contato"
+						title="Procurando um desenvolvedor front-end?"
+						description="Seus problemas acabaram!"
+						backgroundColor="#2D2C2A"
+						backgroundImage="message-icon.png"
+						backgroundPositionX="3%"
+						backgroundPositionY="110%"
+					/>
+				</main>
+			</div>
+		</>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { projects } = await graphSDK.Projects();
-  return {
-    props: {
-      projects,
-    },
-    revalidate: 60 * 60 * 24, // 24 hours,
-  };
+	const { projects } = await graphSDK.Projects();
+	return {
+		props: {
+			projects,
+		},
+		revalidate: 60 * 60 * 24, // 24 hours,
+	};
 };
