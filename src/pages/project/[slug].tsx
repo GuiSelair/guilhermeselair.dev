c4aaffc347d6f797a36fc22cdb41fc1a37f1bed3
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-04-25 22:44:56 -0300

    Styles: Changes in media queries, and little changes

--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -16,101 +16,100 @@ import style from "styles/pages/Project.module.scss";
 import { getThumbnailFilename } from "utils/getThumbnailFilename";
 
 export default function Project({ project }: ProjectQuery) {
-  const projectGallery = useMemo(
-    () => [
-      {
-        src: project.cover.url,
-        id: project.cover.id,
-      }, ...project.gallery.map((image) => ({
-        src: image.url,
-        id: image.id,
-      }))],
-    [project],
-  );
+	const projectGallery = useMemo(
+		() => [
+			{
+				src: project.cover.url,
+				id: project.cover.id,
+			},
+			...project.gallery.map((image) => ({
+				src: image.url,
+				id: image.id,
+			})),
+		],
+		[project]
+	);
 
-  return (
-    <>
-      <SEO
-        title={project.name}
-        image={project.cover.thumbnailToSEO}
-        description={`${project.description.slice(0, 150)}...`}
-      />
-      <Header />
-      <div className={style.container}>
-        <h1>{project.name}</h1>
-        <ProjectCarousel gallery={projectGallery} />
+	return (
+		<>
+			<SEO
+				title={project.name}
+				image={project.cover.thumbnailToSEO}
+				description={`${project.description.slice(0, 150)}...`}
+			/>
+			<Header />
+			<div className={style.container}>
+				<h1>{project.name}</h1>
+				<ProjectCarousel gallery={projectGallery} />
 
-        <div className={style.content}>
-          <h2>DESCRIÇÃO</h2>
-          <ReactMarkdown
-            className={style.markdownContent}
-          >
-            {project.description}
-          </ReactMarkdown>
+				<div className={style.content}>
+					<h2>DESCRIÇÃO</h2>
+					<ReactMarkdown className={style.markdownContent}>
+						{project.description}
+					</ReactMarkdown>
 
-          <div className={style.additionalSectionGridContainer}>
-            <section className={style.tecnologiesUsed}>
-              <h3>PRINCIPAIS TECNOLOGIAS UTILIZADAS</h3>
-              <ul>
-                {project.technologies.map((technology) => (
-                  <li key={technology}>
-                    <Image
-                      src={`/images/thumbnails/${getThumbnailFilename(technology)}`}
-                      width={30}
-                      height={30}
-                      layout="responsive"
-                    />
-                  </li>
-                ))}
-              </ul>
-            </section>
-            <section className={style.viewMore}>
-              <h3>VEJA MAIS</h3>
-              <div>
-                <a
-                  href={project.githubUrl || "#"}
-                  className={style.githubLink}
-                >
-                  REPOSITORIO NO GITHUB
-                  <AiFillGithub />
-                </a>
-                <a
-                  href={project.websiteUrl || "#"}
-                  className={style.websiteLink}
-                >
-                  VEJA A DEMO
-                  <HiOutlineExternalLink />
-                </a>
-              </div>
-            </section>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+					<div className={style.additionalSectionGridContainer}>
+						<section className={style.tecnologiesUsed}>
+							<h3>PRINCIPAIS TECNOLOGIAS UTILIZADAS</h3>
+							<ul>
+								{project.technologies.map((technology) => (
+									<li key={technology}>
+										<Image
+											src={`/images/thumbnails/${getThumbnailFilename(
+												technology
+											)}`}
+											width={30}
+											height={30}
+											layout="responsive"
+										/>
+									</li>
+								))}
+							</ul>
+						</section>
+						<section className={style.viewMore}>
+							<h3>VEJA MAIS</h3>
+							<div>
+								<a href={project.githubUrl || "#"} className={style.githubLink}>
+									REPOSITORIO NO GITHUB
+									<AiFillGithub />
+								</a>
+								<a
+									href={project.websiteUrl || "#"}
+									className={style.websiteLink}
+								>
+									VEJA A DEMO
+									<HiOutlineExternalLink />
+								</a>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { projects } = await graphSDK.SlugsOfProjects();
+	const { projects } = await graphSDK.SlugsOfProjects();
 
-  return {
-    paths: projects.map((project) => ({
-      params: {
-        slug: project.slug,
-      },
-    })),
-    fallback: false,
-  };
+	return {
+		paths: projects.map((project) => ({
+			params: {
+				slug: project.slug,
+			},
+		})),
+		fallback: false,
+	};
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { project } = await graphSDK.Project({ slug: String(params.slug) });
+	const { project } = await graphSDK.Project({ slug: String(params.slug) });
 
-  return {
-    props: {
-      project,
-    },
-    revalidate: 60 * 60 * 24, // 24 hours
-  };
+	return {
+		props: {
+			project,
+		},
+		revalidate: 60 * 60 * 24, // 24 hours
+	};
 };
