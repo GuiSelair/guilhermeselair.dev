8a97dd2f218a0f064f113acd87d6088ea0fdc055
Guilherme Selair <guilherme.lima@getrak.com.br>
2022-05-03 21:24:13 -0300

    Feat: Added LightBox image in Carrousel

--- src/components/pages/project/ProjectCarousel/index.tsx
+++ src/components/pages/project/ProjectCarousel/index.tsx
@@ -1,8 +1,10 @@
+import "react-awesome-lightbox/build/style.css";
 import { Carousel } from "react-responsive-carousel";
 import { isMobile } from "react-device-detect";
 import { HiArrowNarrowLeft, HiArrowNarrowRight } from "react-icons/hi";
+import Lightbox from "react-awesome-lightbox";
 import Image from "next/image";
-import { memo } from "react";
+import { memo, useState } from "react";
 
 import style from "./styles.module.scss";
 
@@ -15,19 +17,106 @@ type IProjectCarousel = {
 };
 
 function ProjectCarousel({ gallery }: IProjectCarousel) {
+	const [lightboxProps, setLightboxProps] = useState({
+		isOpen: false,
+		currentImage: "",
+	});
 	const hasProjectGalleryImages = gallery.length > 1;
+
+	function handleOpenLightbox(imageSrc: string): void {
+		setLightboxProps({
+			isOpen: true,
+			currentImage: imageSrc,
+		});
+	}
+
+	function handleCloseLightBox(): void {
+		setLightboxProps({
+			isOpen: false,
+			currentImage: "",
+		});
+	}
 
 	if (!hasProjectGalleryImages) {
 		return (
+			<>
+				<Carousel
+					showThumbs={false}
+					showStatus={false}
+					showIndicators={false}
+					showArrows={false}
+					className={style.carouselContainer}
+				>
+					{gallery.map((image) => (
+						<div
+							className={style.carouselShowOnlyCoverContainer}
+							key={image.id}
+							onClick={() => handleOpenLightbox(image.src)}
+						>
+							<Image
+								src={image.src}
+								alt={image.alt}
+								layout="responsive"
+								priority
+								width={100}
+								height={100}
+							/>
+						</div>
+					))}
+				</Carousel>
+				<Lightbox
+					image={lightboxProps.currentImage}
+					onClose={handleCloseLightBox}
+				/>
+			</>
+		);
+	}
+	return (
+		<>
 			<Carousel
+				centerMode={!isMobile}
+				centerSlidePercentage={!isMobile ? 59.5 : 65}
+				autoPlay
+				infiniteLoop={!isMobile}
 				showThumbs={false}
+				showArrows
 				showStatus={false}
-				showIndicators={false}
-				showArrows={false}
+				showIndicators
 				className={style.carouselContainer}
+				renderArrowNext={(clickHandler) => (
+					<button
+						className={style.nextImageArrowContainer}
+						type="button"
+						onClick={clickHandler}
+					>
+						<HiArrowNarrowRight />
+					</button>
+				)}
+				renderArrowPrev={(clickHandler) => (
+					<button
+						className={style.previousImageArrowContainer}
+						type="button"
+						onClick={clickHandler}
+					>
+						<HiArrowNarrowLeft />
+					</button>
+				)}
+				renderIndicator={(clickHandler, isSelected, index) => (
+					<button
+						className={style.dot}
+						onClick={clickHandler}
+						data-selected={String(isSelected)}
+						type="button"
+						aria-label={`Indicador ${index}`}
+					/>
+				)}
 			>
 				{gallery.map((image) => (
-					<div className={style.carouselShowOnlyCoverContainer} key={image.id}>
+					<div
+						className={style.carouselImagesContainer}
+						key={image.id}
+						onClick={() => handleOpenLightbox(image.src)}
+					>
 						<Image
 							src={image.src}
 							alt={image.alt}
@@ -39,60 +128,11 @@ function ProjectCarousel({ gallery }: IProjectCarousel) {
 					</div>
 				))}
 			</Carousel>
-		);
-	}
-	return (
-		<Carousel
-			centerMode={!isMobile}
-			centerSlidePercentage={!isMobile ? 59.5 : 65}
-			autoPlay
-			infiniteLoop={!isMobile}
-			showThumbs={false}
-			showArrows
-			showStatus={false}
-			showIndicators
-			className={style.carouselContainer}
-			renderArrowNext={(clickHandler) => (
-				<button
-					className={style.nextImageArrowContainer}
-					type="button"
-					onClick={clickHandler}
-				>
-					<HiArrowNarrowRight />
-				</button>
-			)}
-			renderArrowPrev={(clickHandler) => (
-				<button
-					className={style.previousImageArrowContainer}
-					type="button"
-					onClick={clickHandler}
-				>
-					<HiArrowNarrowLeft />
-				</button>
-			)}
-			renderIndicator={(clickHandler, isSelected, index) => (
-				<button
-					className={style.dot}
-					onClick={clickHandler}
-					data-selected={String(isSelected)}
-					type="button"
-					aria-label={`Indicador ${index}`}
-				/>
-			)}
-		>
-			{gallery.map((image) => (
-				<div className={style.carouselImagesContainer} key={image.id}>
-					<Image
-						src={image.src}
-						alt={image.alt}
-						layout="responsive"
-						priority
-						width={100}
-						height={100}
-					/>
-				</div>
-			))}
-		</Carousel>
+			<Lightbox
+				image={lightboxProps.currentImage}
+				onClose={handleCloseLightBox}
+			/>
+		</>
 	);
 }
 
